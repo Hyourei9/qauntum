bd304f9e240d84dc6e037576af70df0ed51d3dae
Naini Mohamed Reda <mohamed-reda.naini@student-cs.fr>
2025-10-07 11:55:49 +0200

    adding bits

--- qiskit_learn.py
+++ qiskit_learn.py
@@ -5,3 +5,11 @@ from qiskit.result import marginal_distribution
 from qiskit.circuit.library import UGate
 from numpy import pi, random
 
+qubit = QuantumRegister(1, "Q")
+ebit0 = QuantumRegister(1, "A")
+ebit1 = QuantumRegister(1, "B")
+a = ClassicalRegister(1, "a")
+b = ClassicalRegister(1, "b")
+
+protocol = QuantumCircuit(qubit, ebit0, ebit1, a, b)
+
