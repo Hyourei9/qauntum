3ce267c0592dc694b744d3b9af25916dfdae13d6
Naini Mohamed Reda <mohamed-reda.naini@student-cs.fr>
2025-10-07 11:51:27 +0200

    adding imports

--- qiskit_learn.py
+++ qiskit_learn.py
@@ -1,1 +1,7 @@
-import qiskit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit_aer import AerSimulation
+from qiskit.visualization import plot_histogram, array_to_latex
+from qiskit.result import marginal_distribution
+from qiskit.circuit.library import UGate
+from numpy import pi, random
+
